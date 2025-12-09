3cfd17c19200d1ef3831e6a3e49b151b933e7756
Dhootmotor <mis@dhootm.com>
2025-12-09 17:27:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx' // correct relative path
+import './index.css'       // Correct: Same folder
+import App from './App.jsx' // Correct: Same folder
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
